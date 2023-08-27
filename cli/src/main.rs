5fdc8cb50e61d29bcdd139c1f2cc51471f830ef8
Kiana Sheibani <kiana.a.sheibani@gmail.com>
2023-08-26 23:47:23 -0400

    Write code to get auth key

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
+fn get_auth_key() -> Option<String> {
+    env::var("AUTH_KEY").ok()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _auth_key = get_auth_key().expect("Could not find authorization key");
 }
