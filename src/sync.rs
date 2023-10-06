f905dfdbc3621e10e2a7c5af7a83ec9651a2341b
Kiana Sheibani <kiana.a.sheibani@gmail.com>
2023-10-05 22:14:29 -0400

    Remove outdated code

--- src/sync.rs
+++ src/sync.rs
@@ -6,34 +6,6 @@ use crate::error;
 use crate::queries::*;
 use sqlite::*;
 
-// Score calculation
-
-// /// Calculate the collective expected score for each team.
-// fn expected_scores(ratings: &Teams<&mut f64>) -> Vec<f64> {
-//     let qs: Vec<f64> = ratings
-//         .into_iter()
-//         .map(|es| 10_f64.powf(es.iter().map(|x| **x).sum::<f64>() / es.len() as f64 / 400.0))
-//         .collect();
-//     let sumq: f64 = qs.iter().sum();
-//     qs.into_iter().map(|q| q / sumq).collect()
-// }
-
-// /// Adjust the ratings of each player based on who won.
-// fn adjust_ratings(ratings: Teams<&mut f64>, winner: usize) {
-//     let exp_scores = expected_scores(&ratings);
-
-//     ratings
-//         .into_iter()
-//         .zip(exp_scores.into_iter())
-//         .enumerate()
-//         .for_each(|(i, (es, exp_sc))| {
-//             let len = es.len() as f64;
-//             let score = f64::from(winner == i);
-//             es.into_iter()
-//                 .for_each(|e| *e += 40.0 * (score - exp_sc) / len);
-//         })
-// }
-
 // Extract set data
 
 fn get_event_sets(event: EventId, auth: &str) -> Option<Vec<SetData>> {
